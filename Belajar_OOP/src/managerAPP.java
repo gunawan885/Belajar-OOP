869becc04a955d0b584a53786f9148f3a494f783
gunawan885 <gunawanwi376@gmail.com>
2025-01-07 15:00:44 +0700

    membuat super constructor

--- Belajar_OOP/src/managerAPP.java
+++ Belajar_OOP/src/managerAPP.java
@@ -1,11 +1,11 @@
 public class managerAPP {
     public static void main(String[] args) {
 
-        var manager = new Manager();
+        var manager = new Manager("Eko");
         manager.name = "Eko";
         manager.sayHello("Budi");
 
-        var vp = new vicePresident();
+        var vp = new vicePresident("joko");
         vp.name = "Joko";
         vp.sayHello("Budi");
     }
--- Belajar_OOP/src/vicePresident.java
+++ Belajar_OOP/src/vicePresident.java
@@ -1,5 +1,10 @@
 class vicePresident extends Manager {
     
+
+    vicePresident(String name){
+        super(name);
+    }
+
     void sayHello(String name){
         System.out.println("Hello " + name + " , my name is VP " + this.name);
     }
