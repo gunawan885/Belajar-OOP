c5d12be8fa3ee5e965acfb53901ef5d1193b19aa
gunawan885 <gunawanwi376@gmail.com>
2025-01-07 13:36:22 +0700

    menambahkan data pada field dan diprint

--- Belajar_OOP/src/PersonApp.java
+++ Belajar_OOP/src/PersonApp.java
@@ -1,12 +1,14 @@
 public class PersonApp {
     public static void main(String[] args) {
         var person1 = new Person();
+        person1.name = "Eko";
+        person1.adress = "Bandung";
+
+        System.out.println(person1.name);
+        System.out.println(person1.adress);
+        System.out.println(person1.country);
+
         Person person2 = new Person();
         Person person3 = new Person();
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-
     }
 }
